cc88a8a2bc8fcb0eb37cc2b3bc4369ed95bf0d1c
mfellbaum <matthewfellbaum@gmail.com>
2021-03-19 22:10:21 +0000

    Updated for 10kb sliding windows

--- scripts/hstatistics_script.R
+++ scripts/hstatistics_script.R
@@ -5,18 +5,21 @@
 hapcount_table <- read.table("hapcount_chr1_test.hapcount", 
                              fill = T, skip = 1, col.names = c(1:9))
 
+#Calculate H1 statistic
+# H1 = Si = 1, . . .n pi2
+
 # Define loop vectors
 loop.vector.1 <- 1:31
 loop.vector.2 <- c(7,8,9)
 
 # Create hstats vector
-hstats.all.rows <- rep(NA, 31)
+h1.all.rows <- rep(NA, 31)
 
 # Open outer loop for all rows
 for(n in loop.vector.1){
   
   # Define hstats vector for single row to reset in each loop    
-  hstats.single.row <- rep(NA, 3)
+  h1.single.row <- rep(NA, 3)
   
   # Open inner loop for 1 row  
   for(f in loop.vector.2){
@@ -24,17 +27,67 @@ for(n in loop.vector.1){
     r <- hapcount_table[n , f]
     d <- as.numeric(unlist(strsplit(r, ":")))
     j <- ((d[2]/90)^2)*(d[1])
-    hstats.single.row[(f-6)] <- j 
+    h1.single.row[(f-6)] <- j 
   }
   
   # Add calculations to the hstats vector
-  t <- sum(hstats.single.row, na.rm = T)
-  hstats.all.rows[n] <- t
+  t <- sum(h1.single.row, na.rm = T)
+  h1.all.rows[n] <- t
   
 }
 
-hstats.all.rows
+h1.all.rows
 
-file.create("hstats_1Mb_window")
-write.table(hstats.all.rows, "hstats_1Mb_window")
-plot(hstats.all.rows)
+file.create("h1_1Mb_window")
+write.table(hstats.all.rows, "h1_1Mb_window")
+plot(h1.all.rows)
+
+# Calculate H12 statistic
+#  H12 = (p1 + p2)2 + Si>2 pi2 = H1 + 2p1p2
+
+# Define loop vectors
+loop.vector.1 <- 1:31
+loop.vector.2 <- c(7,8,9)
+loop.vector.3 <- c(7,8)
+
+# Create hstats vector
+h12.all.rows <- rep(NA, 31)
+
+# Open outer loop for all rows
+for(n in loop.vector.1){
+  
+  # Define hstats vector for single row to reset in each loop    
+  h12.single.row <- rep(NA, 3)
+  
+  # Open inner loop for 1 row  
+  for(f in loop.vector.2){
+    if(hapcount_table[n , f] == "") {break}
+    r <- hapcount_table[n , f]
+    d <- as.numeric(unlist(strsplit(r, ":")))
+    j <- ((d[2]/90)^2)*(d[1]) 
+    
+# Trying to add the + 2*hap1*hap2 using loops but don't think this will work.    
+    for(x in loop.vector.3){
+        v <- hapcount_table[n, x]
+        a <- rep(NA, 2)
+        a <- as.numeric(unlist(strsplit(v, ":")))
+        c <- sum(a)
+        }
+     b <- j + 2*c
+     hap12[f-6] <- b
+      
+    
+    h12.single.row[(f-6)] <- j 
+  }
+  
+  # Add calculations to the hstats vector
+  t <- sum(h12.single.row, na.rm = T)
+  h12.all.rows[n] <- t
+  
+}
+
+h12.all.rows
+
+file.create("h12_1Mb_window")
+write.table(hstats.all.rows, "h12_1Mb_window")
+plot(h12.all.rows)
