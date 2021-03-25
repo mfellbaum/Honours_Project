b5ec04cf19a913ffe7c25bcf9a6280197932c704
mfellbaum <matthewfellbaum@gmail.com>
2021-03-25 12:59:33 +0000

    Updated to make 10kb sliding windows

--- scripts/bedfile_script.R
+++ scripts/bedfile_script.R
@@ -1,19 +1,19 @@
 chromStart <- seq(from = 1, 
-                  to = 29990000,
-                  by = 9000)
+                  to = 29991000,
+                  by = 1000)
 
 chromEnd <- seq(from = 10000,
                 to = 30000000,
-                by = 9000)
+                by = 1000)
 
-chrom <- rep("chrom 1", each = length(chromStart))
+chrom <- rep("1", each = length(chromStart))
 
 bedfile <- data.frame(chrom,chromStart,chromEnd)
 
 cat("track name=\"atsweeps\" description=\"BED file for hapcount\"\n",
-    file = "hapcount_BED_file_1kb.bed")
+    file = "hapcount_BED_file_10kb.bed")
 
-write.table(bedfile, file = "hapcount_BED_file_1kb.bed",
+write.table(bedfile, file = "hapcount_BED_file_10kb.bed",
             row.names = F, 
             col.names = F,
             quote = F,
