9db13e0f0bef39a275492e21dc5b0282afd9a16e
mfellbaum <matthewfellbaum@gmail.com>
2021-04-06 11:00:10 +0100

    Updated the hapfreq section

--- scripts/hstatistics_script.R
+++ scripts/hstatistics_script.R
@@ -1,12 +1,12 @@
 ## Calculate H statistics for all rows in the hapcount file using 2 loops.
 
-# Need to update the hapcount file name.
+# Need to update the hapcount file name for different window sizes.
 
 
 # All H stats in 1 script
 
 # Open hapcount table into R 
-hapcount_table <- read.table("hapcount_chr1_10kb.hapcount", 
+hapcount_table <- read.table("hapcount_chr1_20kb.hapcount", 
                              fill = T, skip = 1, col.names = c(1:13))
 rows <- nrow(hapcount_table)
 columns <- ncol(hapcount_table)
@@ -44,59 +44,48 @@ for(n in loop.vector.1){
     j <- ((d[2]/90)^2)*(d[1])
     h1.single.row[(f-6)] <- j 
     
-    
-    # Sort hapfreq and multiplicity 
+    # Define hapfreq and multiplicity vectors for 1 row
     hapfreq[f-6] <- d[2]
-    hapfreq <- sort(hapfreq, decreasing = T)
-    
-    # This doesn't work yet - something going wrong with the reverse over loops
     multiplicity[f-6] <- d[1]
-    multiplicity <- rev(multiplicity)
-    multiplicity <- na.omit(multiplicity)
-    
-    
-    # Define 2 most common haplotypes for 1 row 
-    hapfreq1 <- 0
-    hapfreq2 <- 0
-    
-    hapfreq1 <- hapfreq[1]
-    if(multiplicity[1] == 1) {hapfreq2 <- hapfreq[2]}
-    else {hapfreq2 <- hapfreq[1]}
      
   }
   
-  # Calculate H1 for all rows
+  # Reverse hapfreq and multiplicity vectors 
+  hapfreq <- rev(hapfreq)
+  hapfreq <- hapfreq[!is.na(hapfreq)]
+  multiplicity <- rev(multiplicity)
+  multiplicity <- multiplicity[!is.na(multiplicity)]
+  
+  # Define 2 most common haplotypes for 1 row 
+  hapfreq1 <- 0
+  hapfreq2 <- 0
+  
+  hapfreq1 <- hapfreq[1]
+  if(multiplicity[1] == 1) {hapfreq2 <- hapfreq[2]}
+  else {hapfreq2 <- hapfreq[1]}
+  
+  # Calculate H1, H12, H2 and H2/H1 for all rows
   h1[n] <- sum(h1.single.row, na.rm = T)
-  
-  # Calculate H12 for all rows   
   h12[n] <- h1[n] + 2*(hapfreq1/90)*(hapfreq2/90)
-  
-  # Calculate H2 for all rows  
   h2[n] <- h1[n]-((hapfreq1/90)^2)
-  
-  # Calculate H2/H1 for all rows  
   h21[n] <- h2[n]/h1[n]
 }
 
 
 
 
-file.create("h1_10kb_window")
-write.table(h1, "h1_10kb_window")
+file.create("h1_20kb_window")
+write.table(h1, "h1_20kb_window")
 
-file.create("h12_10kb_window")
-write.table(h12, "h12_10kb_window")
+file.create("h12_20kb_window")
+write.table(h12, "h12_20kb_window")
 
-file.create("h21_10kb_window")
-write.table(h21, "h21_10kb_window")
+file.create("h21_20kb_window")
+write.table(h21, "h21_20kb_window")
 
-plot(h1, pch = 20, xlab = "Window", main = "H1 statistics for 10kb fixed windows")
-plot(h12, pch = 20, xlab = "Window", main = "H12 statistics for 10kb fixed windows")
-plot(h21, pch = 20, xlab = "Window", main = "H2/H1 statistics for 10kb fixed windows")
+plot(h1, pch = 20, xlab = "Window", main = "H1 statistics for 20kb fixed windows")
+plot(h12, pch = 20, xlab = "Window", main = "H12 statistics for 20kb fixed windows")
+plot(h21, pch = 20, xlab = "Window", main = "H2/H1 statistics for 20kb fixed windows")
 
 
 
-# Sketchy way to define the hapfreq - think it would work but not the proper way to do it
-#hapfreq2 <- 0
-#hapfreq2 <- hapfreq1
-#hapfreq1 <- d[2]
